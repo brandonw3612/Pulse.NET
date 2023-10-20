59f4b44cd4ff6fce2c61a8d7fbf0732ff70aa71a
Brandon Wong <29965003+brandonw3612@users.noreply.github.com>
2023-10-20 16:29:53 +0800

    fix: add access lock on the Invalidate action in ParameterStateMachine

--- src/Pulse.NET/ParameterStateMachine.cs
+++ src/Pulse.NET/ParameterStateMachine.cs
@@ -1,13 +1,41 @@
 ﻿namespace Pulse;
 
+/// <summary>
+/// State machine for action/task parameters used in this library, maintaining its state of validity.
+/// </summary>
+/// <typeparam name="T">Type of the parameter.</typeparam>
 internal class ParameterStateMachine<T>
 {
+    /// <summary>
+    /// Real value of the parameter.
+    /// </summary>
     private T? _value;
+    
+    /// <summary>
+    /// Whether the parameter is currently set as valid.
+    /// </summary>
     private bool _isValid;
+    
+    /// <summary>
+    /// Access lock for the parameter.
+    /// </summary>
     private readonly object _lock = new();
 
-    public void Invalidate() => _isValid = false;
+    /// <summary>
+    /// Invalidates the parameter.
+    /// </summary>
+    public void Invalidate()
+    {
+        lock (_lock)
+        {
+            _isValid = false;
+        }
+    }
     
+    /// <summary>
+    /// Sets the value of the parameter.
+    /// </summary>
+    /// <param name="value"></param>
     public void SetValue(T value)
     {
         lock (_lock)
@@ -17,6 +45,11 @@ internal class ParameterStateMachine<T>
         }
     }
     
+    /// <summary>
+    /// Tries to get the value of the parameter.
+    /// </summary>
+    /// <param name="value">If the method succeeded, the value of the parameter.</param>
+    /// <returns>Whether current parameter is valid.</returns>
     public bool TryGetValue(out T value)
     {
         lock (_lock)
